2c1c734d0882684738718084dc91a742ed5598c6
Nevin <nevinzheng0921@gmail.com>
2025-08-24 00:43:33 -0400

    functioning app with margin errors

---
+++ app.js
@@ -0,0 +1,65 @@
+const p1Button = document.querySelector('#p1Add')
+const p2Button = document.querySelector('#p2Add')
+
+const p1Display = document.querySelector('#p1Display')
+const p2Display = document.querySelector('#p2Display')
+
+const winningScoreSelect = document.querySelector('#playTo')
+
+const resetBtn = document.querySelector('#reset')
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
+
+
+p1Button.addEventListener('click', function() {
+    if(!isGameOver){
+        p1Score += 1
+        p1Display.textContent = p1Score
+        if(p1Score === winningScore){
+            isGameOver = true;
+            p1Display.classList.add('has-text-success')
+            p2Display.classList.add('has-text-danger')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        }
+    }
+})
+
+p2Button.addEventListener('click', function() {
+    if(!isGameOver){
+        p2Score += 1
+        p2Display.textContent = p2Score
+        if(p2Score === winningScore){
+            isGameOver = true;
+            p2Display.classList.add('has-text-success')
+            p1Display.classList.add('has-text-danger')
+            p2Button.disabled = true;
+            p1Button.disabled = true;
+        }
+    }
+})
+
+winningScoreSelect.addEventListener('change',function(){
+    winningScore = parseInt(this.value);
+    reset()
+})
+
+resetBtn.addEventListener('click', reset)
+
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p1Display.textContent = p1Score;
+    p1Button.disabled = false;
+    
+    p2Score = 0;
+    p2Display.textContent = p2Score;
+    p2Button.disabled = false;
+
+    p1Display.classList.remove('has-text-success', 'has-text-danger')
+    p2Display.classList.remove('has-text-success', 'has-text-danger')
+}
