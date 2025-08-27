0471bfaf4790fb69de576da1038969ebb5115216
Nevin <nevinzheng0921@gmail.com>
2025-08-27 00:20:25 -0400

    add nextRound button - works on round 1 but malfunction afterward

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const winningScoreSelect = document.querySelector('#playTo')
 const resetBtn = document.querySelector('#reset')
+const nextRound = document.querySelector('#nextRound')
+const roundToSelect = document.querySelector('#roundTo')
 
 const p1 = {
     score: 0,
@@ -16,23 +18,39 @@ const p2 = {
     display: document.querySelector('#p2Display'),
     overall: document.querySelector('#p2Overall')
 }
-
+let roundScore = 3
 let winningScore = 3;
 let isGameOver = false;
-
+let isRoundOver = false;
+let winner = null
 function updateScore(player, opponent){
-    if(!isGameOver){
+    if(!isRoundOver){
         player.score += 1
         player.display.textContent = player.score
         if(player.score === winningScore){
-            isGameOver = true;
+            isRoundOver = true;
             player.display.classList.add('has-text-success')
             opponent.display.classList.add('has-text-danger')
             player.button.disabled = true;
             opponent.button.disabled = true;
+            winner = player
         }
     }
 }
+nextRound.addEventListener('click', function(){
+    if(!isRoundOver){
+        if(winner){
+        winner.overallScore += 1
+        winner.overall.textContent = winner.overallScore
+        winner = null
+        reset()
+        if
+        }
+    }else{
+        alert('congrates on thw winner')
+    }
+    
+})
 
 p1.button.addEventListener('click', function() {
     updateScore(p1, p2)
@@ -46,17 +64,22 @@ winningScoreSelect.addEventListener('change',function(){
     winningScore = parseInt(this.value);
     reset()
 })
+roundToSelect.addEventListener('change', function(){
+    roundScore = parseInt(this.value);
+    reset()
+})
 
 resetBtn.addEventListener('click', reset)
 
 
 function reset(){
     isGameOver = false;
+    isRoundOver = false;
     for(let p of [p1,p2]){
         p.score = 0;
+        p.overall = 0;
         p.display.textContent = p.score;
         p.button.disabled = false;
-        p.display.classList.remove('has-text-success', 'has-text-danger')
-
+        p.display.classList.remove('has-text-success', 'has-text-danger');
     }
 }
